aba8b8fd43f801c476502ef953f377aae173af04
formation_gep <chalendard.remy@gmail.com>
2020-01-29 15:01:21 +0100

    Exercie_1 POO

--- tableau.php
+++ tableau.php
@@ -37,10 +37,10 @@ $personne['Nom'] = 'Martin';
 $personne['Prenom'] = 'Monique';
 $personne['Age'] = 50;
 
-echo 'Age : '.$personne['Age'].'<nbr>';
+echo ' Age : '.$personne['Age'].'<nbr>';
 
 //3 constantes de type tableau
-const couleurs = array('rouge' , 'vert', 'noir');
+const couleurs = array('rouge' , ' vert ', 'noir');
 echo couleurs[1];
 
 define ('COLORS', array (
@@ -76,3 +76,129 @@ foreach ($tab as &$val) {
 
 $tableau = array ('Al' => 'Jean' , 'B4' => 'Robert', 3=> 'Paul', 'toto' => 'Joe', 'H' => 'Alain');
 print_r($tableau);
+
+$tableau = array('Al' =>'jean', 'B4' => 'Robert',3=> 'Paul', 'Toto' => 'Joe', 'H'=> 'Alain');
+$taille = count ($tableau);
+echo 'La taille du tableau est :  '.$taille;
+
+$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+if (in_array('Robert', $tableau)) {
+    echo ' , Robert est dans le tableau ';
+}
+
+$tableau = array ('10','33','55','78');
+if (in_array('33', $tableau, true)) {
+    echo ' , 33 est dans le tableau';
+}else{
+    echo ' , 33 est pas dans le tableau ' ;
+}
+
+$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+if (array_key_exists (3 , $tableau)) {
+    echo ' , la clef 3 est dans le tableau';
+}
+
+$tableau = array ('Jean' , 'Robert', 'Paul', 'Joe' , 'Alain');
+if (array_key_exists (4, $tableau)) {
+echo ' , la clef 4 est dans le tableau' .$tableau [4];
+}
+
+$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+sort ($tableau);
+foreach ($tableau as $cle =>$valeur) {
+ echo 'Cle :  ' .$cle. ', valeur : ' .$valeur. '<br>' ;
+}
+
+$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+asort ($tableau);
+foreach ($tableau as $cle =>$valeur) {
+ echo 'Cle :  ' .$cle. ', valeur : ' .$valeur. '<br>' ;
+}
+
+$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+rsort ($tableau);
+foreach ($tableau as $cle =>$valeur) {
+ echo 'Cle :  ' .$cle. ', valeur : ' .$valeur. '<br>' ;
+}
+
+//$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+//asort ($tableau);
+//foreach ($tableau as $cle =>$valeur) {
+// echo 'Cle :  ' .$cle. ', valeur : ' .$valeur. '<br>' ;
+//}
+
+//$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+//ksort ($tableau);
+//foreach ($tableau as $cle =>$valeur) {
+// echo 'Cle :  ' .$cle. ', valeur : ' .$valeur. '<br>' ;
+//}
+
+//$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+//krsort ($tableau);
+//foreach ($tableau as $cle =>$valeur) {
+// echo 'Cle :  ' .$cle. ', valeur : ' .$valeur. '<br>' ;
+//}
+
+//$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+//$cle_element = array_search ('Robert' , $tableau);
+//echo "la cle de l'élément recherchez est : " .$cle_element;
+
+//$tableau = array ('Al'=>'Jean', 'B4' => 'Robert' ,3=> 'Paul', 'Toto' => 'Joe', 'H' => 'Alain');
+//$cle_element = array_search('Robert, $tableau');
+//echo "<la clé de l'élément recherché est :  ".$cle_element;
+
+//$ensemble = "1;2;3;4;5";
+//explode($tableau, $ensemble);
+//$tableau = explode (";" , $ensemble);
+//echo "La premiere valeur du tableau est : " .$tableau[0]."<br />";
+//echo "la derniere valeur du tableau est : " .$tableau[count($tableau)-1];
+
+$ensemble = "1-2-3-4-5";
+$tableau = explode("-" , $ensemble);
+echo " ,  Les valeurs du tableau sont : ";
+foreach ($tableau as $valeur) {
+    echo $valeur.";";
+}
+
+$tableau = array ("   Jean","Robert","Paul");
+implode($ensemble, $tableau);
+foreach ($tableau as $valeur) {
+   echo $valeur. ";";
+
+//$tableau = array ("   Jean","Robert","Paul");
+//$noms=implode(";" , $tableau);
+//echo "les noms sont : ".$noms;
+
+//$ensemble = "1;2;3;4;5";
+//$tableau=str_split($ensemble,2);
+//echo " , Les elements sont : ";
+//foreach ($tableau as $valeur) {
+//    echo $valeur. "  ";   
+//}
+
+//$tableau = array ("   Jean","Robert","Paul");
+//array_push ($tableau, "Joe"," Alain");
+//echo $tableau[4];
+
+//$tableau =array ("Jean","Robert","Paul");
+//$nom = array_pop ($tableau);
+//echo "Le nom supprimé est : " .$nom ;
+
+//$tableau =array ("Jean","Robert","Paul");
+//echo "Avant la suppression , le nombre d'éléments dans le tableau est : ".count($tableau)."<br />";
+//$nombre_elements = count($tableau);
+//for ($i=0;$i<nombre_elements;$i++) {
+//    $nom = array_pop($tableau);
+//}
+//echo "le dernier nom supprimé est : " .$nom."<br />";
+//echo "Apres suppression, nb élements : " .count ($tableau);
+}
+
+$tableau = array ("Jean","Robert","Paul");
+$cle_aleatoire = array_rand ($tableau);
+echo "Nom sélectionné au hasard : ".$tableau [$cle_aleatoire];
+
+$tableau = array ("Jean","Robert","Paul");
+$tableau_cle_aleatoire = array_rand($tableau,2);
+echo "Premier nom : " .$tableau[$tableau_cle_aleatoire[0]]."<br>";
+echo "Second nom : " .$tableau[$tableau_cle_aleatoire[1]];
